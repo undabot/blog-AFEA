4af094ff9ad99310c41e5214121106d25dcda276
Azzaro <azzaro.mujic@undabot.com>
2018-06-14 14:52:47 +0200

    added coverage

--- AFEA-EndProject/AFEA-EndProjectTests/AFEA_EndProjectTests.swift
+++ AFEA-EndProject/AFEA-EndProjectTests/AFEA_EndProjectTests.swift
@@ -22,7 +22,6 @@ class AFEA_EndProjectTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
